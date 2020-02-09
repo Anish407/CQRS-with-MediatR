8d27879b4864fb0a13bffa6f713147657d8831bb
Anish Aravind <anish.arvind407@gmail.com>
2020-02-09 19:23:28 +0530

    MediatR added Command and message base added

---
+++ CleanArchCQRS/CleanArch.Infra.Bus/InMemoryBus.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Domain.Core.Bus;
+using CleanArch.Domain.Core.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Bus
+{
+   public sealed class InMemoryBus:IMediatorHandler
+    {
+        public InMemoryBus(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+
+        public IMediator Mediator { get; }
+
+
+        public Task SendCommand<T>(T command) where T: Command
+        {
+            return Mediator.Send(command);
+        } 
+    }
+}
